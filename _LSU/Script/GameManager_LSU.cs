799d8b1ac719f8f6c7bc11d9d9ab9092aab8391d
Gamja <tiger_7@naver.com>
2025-07-12 02:02:15 +0900

    타이틀/엔딩

---
+++ _LSU/Script/GameManager_LSU.cs
@@ -0,0 +1,180 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager_LSU : MonoBehaviour
+{
+    GameEnding gameEndingScript;
+
+    [Header("반죽 단계 관련")]
+    [SerializeField] GameObject doughGroup; //도우 게임오브젝트 그룹
+    [SerializeField] Image doughTimerBar; //도우 타이머 바
+    [SerializeField] float doughTime; //도우 타이머 시간
+    [SerializeField] float doughTimer; //도우 타이머 시간
+    [SerializeField] bool hasDough1 = false; //재료1 다 모았는지 여부
+    [SerializeField] bool hasDough2 = false; //재료2 다 모았는지 여부
+    [SerializeField] bool hasDough3 = false; //재료3 다 모았는지 여부
+
+    [Header("진행도 관련")]
+    [SerializeField] Slider progressSlider; //진행도 슬라이더
+    [SerializeField] float progressTime; //진행도 시간 = 최대 플레이 시간
+    [SerializeField] float progressTimer; //진행도 타이머
+    public int stageID = 1; //스테이지 1, 2, 3
+    bool isGameOver = false; //게임종료 여부
+
+    [Header("체력 및 데미지 관련")]
+    [SerializeField] GameObject hpGroup; //체력 게임오브젝트 그룹
+    [SerializeField] Image hpBar; //체력바 이미지
+    [SerializeField] float maxHp; //최대 체력
+    [SerializeField] float currentHp; //현재 체력
+    [SerializeField] bool onFire = false; //불 활성화 여부
+    [SerializeField] float fireDamage = 0f; //불 데미지
+    [SerializeField] float fireDamageDuration = 5f; //불 데미지 지속시간
+
+
+    private void Awake()
+    {
+        gameEndingScript = GetComponent<GameEnding>();
+
+        doughTimer = doughTime;
+        maxHp = 100;
+        currentHp = 100;
+        hpBar.fillAmount = 1;
+    }
+
+    private void Update()
+    {
+        if (isGameOver)
+            return;
+        
+        if(stageID == 1)
+            DoughTimerUpdate();
+
+        ProgressTimerUpdate();
+
+
+        //장애물 데미지 테스트용
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //장애물 피격 시 -10
+            PlayerHpDecrease(10, "Obstacle");
+        }
+
+        //불 데미지 테스트용
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            //불 데미지 실행
+            PlayerHpDecrease(0, "Fire");
+        }
+
+        //불 데미지 실행
+        if (onFire && fireDamageDuration >= 0)
+        {
+            FireDamage();
+        }
+
+        //게임오버
+        if (currentHp <= 0 && !isGameOver)
+        {
+            isGameOver = true;
+            gameEndingScript.EndGame();
+        }
+    }
+
+    //반죽 업데이트
+    private void DoughTimerUpdate()
+    {
+        doughTimer -= Time.deltaTime;
+        doughTimerBar.fillAmount = doughTimer / doughTime;
+
+        if (doughTimerBar.fillAmount <= 0.001f)
+        {
+            if(!hasDough1 || !hasDough2 || !hasDough3)
+            {
+                isGameOver = true;
+                gameEndingScript.EndGame();
+            }
+            else
+            {
+                stageID = 2;
+                doughGroup.SetActive(false);
+                hpGroup.SetActive(true);
+                //스테이지 2로 전환
+            }
+        }
+    }
+
+    //진행도 업데이트
+    private void ProgressTimerUpdate()
+    {
+        progressTimer += Time.deltaTime;
+        progressSlider.value = progressTimer / progressTime;
+
+        if(progressSlider.value <= 0.3 && stageID == 1)
+        {
+            stageID = 1;
+        }
+        else if(progressSlider.value <= 0.6)
+        {
+            stageID = 2;
+        }
+        else if(progressSlider.value <= 0.99 )
+        {
+            stageID = 3;
+        }
+        else
+        {
+            stageID = 4;
+        }
+    }
+
+    //불 데미지
+    private void FireDamage()
+    {
+        currentHp -= fireDamage * Time.deltaTime;
+        hpBar.fillAmount = currentHp / maxHp;
+        fireDamageDuration -= Time.deltaTime;
+
+        //지속시간 종료
+        if(fireDamageDuration <= 0)
+        {
+            onFire = false;
+            fireDamage = 0;
+            fireDamageDuration = 0;
+        }
+    }
+
+    //체력 감소 -> 플레이어  OnTriggerEnter에서 호출
+    public void PlayerHpDecrease(float damage, string type)
+    {
+        if(stageID == 1)
+        {
+            //재료 잃기?
+            //재료 배열 혹은 리스트에서 랜덤으로 해당되는 재료의 갯수를 차감
+            //Random.Range(0, );
+        }
+
+        if (stageID != 1)
+        {
+            if (type == "Obstacle")
+            {
+                currentHp -= damage;
+                hpBar.fillAmount = currentHp / maxHp;
+            }
+            else if (type == "Fire")
+            {
+                onFire = true;
+                fireDamage += 2.0f;
+                fireDamageDuration = 5.0f;
+            }
+        }
+        
+    }
+
+    //플레이어가 낙하했을 경우에 바로 게임오버.
+    //해당 함수를 사용할 필요없이 데미지 함수에서 데미지의 크기를 키워서 줘도 됨.
+    public void PlayerDroped()
+    {
+        isGameOver = true;
+        gameEndingScript.EndGame();
+    }
+}
---
+++ _LSU/Script/TitleManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.anyKey)
+        {
+            SceneManager.LoadScene("Game_LSU"); //씬 로드 방식
+            //titleUI.SetActive(false); //UI 오브젝트 비활성화
+            Debug.Log("게임화면으로 이동");
+        }
+    }
+}
