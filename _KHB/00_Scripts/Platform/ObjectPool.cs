2885ca5256f8e9474a0fa0ed89e1333d61a9df4b
gyunabry <hanbin3471@gmail.com>
2025-07-12 16:31:40 +0900

    장애물 변경
    - 장애물 스프라이트 추가

---
+++ _KHB/00_Scripts/Platform/ObjectPool.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab;
+    [SerializeField] private int poolSize;
+    
+    private Queue<GameObject> pool = new Queue<GameObject>(); // 오브젝트 풀링을 위해 큐 선언
+
+    private void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(platformPrefab, this.transform);
+            obj.SetActive(false); // 첫 생성 시 비활성화
+            pool.Enqueue(obj); // Pool에 삽입
+        }
+    }
+
+    // Pool에서 오브젝트를 반환
+    public GameObject GetObject(Vector3 position)
+    {
+        GameObject obj = pool.Count > 0 ? pool.Dequeue() : Instantiate(platformPrefab, this.transform);
+        obj.transform.position = position;
+        obj.SetActive(true);
+
+        // PlatformMover가 있다면 ObjectPool을 넘겨줌
+        //PlatformMover mover = obj.GetComponent<PlatformMover>();
+        //if (mover != null)
+        //{
+        //    mover.Init(this);
+        //}
+
+        return obj;
+    }
+
+    // 오브젝트를 Pool로 반환
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
---
+++ _KHB/00_Scripts/Segments/Segment.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Segment : MonoBehaviour
+{
+    public SegmentType segmentType;
+    public Transform startPoint;
+    public Transform endPoint;
+}
